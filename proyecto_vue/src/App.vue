78e4a31d4c3147bf87fbd7674f788dc83c4b0365
ITLAZ <juan.tapia.l@ucb.edu.bo>
2023-10-14 23:43:14 -0400

    navbar, aun no sirve el responsive

--- proyecto_vue/src/App.vue
+++ proyecto_vue/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <LoginSignUp/>
+    <NavigationBar/>
   </div>
 </template>
 
 <script>
 import LoginSignUp from './components/LoginSignUp.vue'
+import NavigationBar from './components/NavigationBar.vue'
 
 export default {
   name: 'App',
   components: {
-    LoginSignUp
+    LoginSignUp,
+    NavigationBar
   }
 }
 </script>
