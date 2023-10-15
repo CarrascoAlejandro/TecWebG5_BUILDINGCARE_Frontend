cf2ec34a9b9c414fd61a6a5a1b5827a131352d34
ITLAZ <juan.tapia.l@ucb.edu.bo>
2023-10-14 22:28:27 -0400

    login y nuevas dependencias

--- proyecto_vue/src/App.vue
+++ proyecto_vue/src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <LoginSignUp/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginSignUp from './components/LoginSignUp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginSignUp
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
 </style>
