44c48c74616c7898a3f2829e928cb980b7b5170b
ITLAZ <juan.tapia.l@ucb.edu.bo>
2023-11-26 21:10:48 -0400

    navbar responsive

--- proyecto_vue/src/components/NavigationBar.vue
+++ proyecto_vue/src/components/NavigationBar.vue
@@ -1,55 +1,63 @@
 <template>
-    <div class="nav-bar">
-        <nav>
-            <div class="logo">
-                <img src="@/assets/logos/bcareHorizontalBlanco.png" alt="logo" />
-            </div>
-            <div class="menu-toggle" @click="toggleMenu">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-            </div>
-            <ul class="nav-links" :class="{ 'nav-active': isMenuOpen }">
-                <li v-for="item in navItems" :key="item.name" :class="{ 'nav-active': activeNavItem === item.name }">
-                    <a @click="setActiveNavItem(item.name)">{{ item.label }}</a>
-                </li>
-            </ul>
+    <div class="nav-container">
+      <div class="nav-bar">
+        <div class="menu-toggle" @click="toggleMenu">
+          <span class="bar bar-top" :class="{ 'bar-top-open': isMenuOpen }"></span>
+          <span class="bar bar-mid" :class="{ 'bar-mid-open': isMenuOpen }"></span>
+          <span class="bar bar-bottom" :class="{ 'bar-bottom-open': isMenuOpen }"></span>
+        </div>
+        <nav class="nav-links" :class="{ 'nav-open': isMenuOpen }">
+          <div class="logo">
+            <img src="@/assets/logos/bcareHorizontalNegro.png" alt="logo" />
+          </div>
+          <li
+            v-for="item in navItems"
+            :key="item.name"
+            :class="{ 'nav-active': activeNavItem === item.name }"
+          >
+            <a @click="setActiveNavItem(item.name)">{{ item.label }}</a>
+          </li>
         </nav>
+      </div>
     </div>
-</template>
+  </template>
 
 <script>
 export default {
-    data() {
-        return {
-            activeNavItem: "about",
-            isMenuOpen: false,
-            navItems: [
-                { name: "about", label: "Sobre Nosotros" },
-                { name: "blog", label: "Anuncios" },
-                { name: "requests", label: "Solicitudes" },
-                { name: "houses", label: "Propiedades" },
-                { name: "logOut", label: "Cerrar Sesión" },
-            ],
-        };
+  data() {
+    return {
+      activeNavItem: "about",
+      isMenuOpen: false,
+      navItems: [
+        { name: "about", label: "Sobre Nosotros" },
+        { name: "blog", label: "Anuncios" },
+        { name: "requests", label: "Solicitudes" },
+        { name: "houses", label: "Propiedades" },
+        { name: "logOut", label: "Cerrar Sesión" },
+      ],
+    };
+  },
+  methods: {
+    setActiveNavItem(item) {
+      this.activeNavItem = item;
+      this.closeMenu();
+      if (item === "blog") {
+        this.$router.push("/blogView");
+      } else if (item === "requests") {
+        this.$router.push("/paymentsView");
+      } else if (item === "houses") {
+        this.$router.push("/propertyView");
+      } else if (item === "logOut") {
+        this.$router.push("/login");
+      }
     },
-    methods: {
-        setActiveNavItem(item) {
-            this.activeNavItem = item;
-            if (item === 'blog'){
-                this.$router.push("/blogView");
-            } else if (item === 'requests'){
-                this.$router.push("/paymentsView");
-            } else if (item === 'houses'){
-                this.$router.push("/propertyView");
-            } else if (item === 'logOut'){
-                this.$router.push("/login");
-            }
-        },
-        toggleMenu() {
-            this.isMenuOpen = !this.isMenuOpen;
-        },
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
     },
+    closeMenu() {
+      this.isMenuOpen = false;
+    },
+  },
 };
 </script>
 
@@ -57,184 +65,164 @@ export default {
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600&display=swap");
 
 * {
-    font-family: "Poppins", sans-serif;
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-.logo img {
-    width: 100%;
+.logo {
+  position: relative;
+
+  img {
+    width: auto;
     height: auto;
     max-width: 200px;
+    transition: all 0.5s ease-in-out;
+  }
+
+  &:hover img {
+    opacity: 0; // O cualquier otra propiedad que haga que la imagen desaparezca
+    transition: all 0.5s ease-in-out;
+  }
+
+  &:hover {
+    cursor: pointer;
+    background-image: url("../assets/logos/bcareHorizontalSand.png");
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
 }
 
 nav {
-    background: #22abb3;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    color: #ffff;
-    padding: 1vh;
+  background: #498c79;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: #101e26;
+  padding: 1vh;
 }
 
 .nav-links {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    text-transform: uppercase;
-    list-style: none;
-    background-color: #22abb3;
-    position: absolute;
-    top: 100%;
-    left: 0;
-    transition: all 0.5s ease-in-out;
-    transform: translateY(-100%);
-    opacity: 0;
-    z-index: -1;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
 }
-
 .nav-links li {
-    width: 100%;
-    text-align: center;
-    margin-bottom: 1rem;
+  width: 100%;
+  text-align: center;
+  list-style: none;
 }
 
 .nav-links a {
-    display: block;
-    text-transform: uppercase;
-    text-decoration: none;
-    color: #ffff;
-    border-bottom: 2px solid transparent;
-    transition: 0.5s ease;
-    transform: translateX(0%);
+  display: block;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: #101e26;
+  transition: 0.5s ease;
+  transform: translateX(0%);
 }
 
 .nav-links .nav-active a {
-    border-bottom: 2px solid #ffff;
-    color: #ffff;
-    letter-spacing: 5px;
+  color: #101e26;
+  letter-spacing: 5px;
 }
 
 .nav-links a:hover {
-    box-shadow: inset 0 -2px 0 #ffff;
-    color: #ffff;
-    letter-spacing: 5px;
+  color: #f2d1b3;
+  letter-spacing: 5px;
+}
+.nav-bar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
-@media only screen and (min-width: 760px) {
-    .nav-links {
-        position: relative;
-        opacity: 1;
-        z-index: 1;
-        flex-direction: row;
-        justify-content: space-around;
-        align-items: center;
-        width: auto;
-        background-color: transparent;
-        transform: translateY(0%);
-    }
-
-    .nav-links li {
-        margin-bottom: 0;
-    }
-
-    .nav-links a {
-        border-bottom: none;
-        margin: 0 1rem;
-    }
-
-    .nav-links .nav-active a {
-        border-bottom: none;
-        color: #ffff;
-        letter-spacing: 5px;
-    }
-
-    .nav-links a:hover {
-        box-shadow: none;
-        color: #ffff;
-        letter-spacing: 5px;
-    }
-
-    .menu-toggle {
-        display: none;
-    }
-}
-
-@media only screen and (max-width: 760px) {
-    nav {
-        justify-content: space-between;
-        padding: 0 5vw;
-    }
-
-    .menu-toggle {
-        display: block;
-    }
-
-    .nav-links {
-        position: absolute;
-        top: 100%;
-        left: 0;
-        width: 100%;
-        opacity: 1;
-        z-index: 1;
-        background-color: #22abb3;
-        transform: translateY(-100%);
-        transition: all 0.5s ease-in-out;
-    }
-
-    .nav-links li {
-        width: 100%;
-        text-align: center;
-        margin-bottom: 1rem;
-    }
-
-    .nav-links a {
-        display: block;
-        text-transform: uppercase;
-        text-decoration: none;
-        color: #ffff;
-        border-bottom: 2px solid transparent;
-        transition: 0.5s ease;
-        transform: translateX(0%);
-    }
-
-    .nav-links .nav-active a {
-        border-bottom: 2px solid #ffff;
-        color: #ffff;
-        letter-spacing: 5px;
-    }
-
-    .nav-links a:hover {
-        box-shadow: inset 0 -2px 0 #ffff;
-        color: #ffff;
-        letter-spacing: 5px;
-    }
-
-    .nav-active {
-        transform: translateY(0%);
-    }
-
+.menu-toggle {
+    display: none;
+    flex-direction: column;
+    cursor: pointer;
+    transition: all 0.5s ease-in-out;
+  
     .bar {
-        width: 25px;
-        height: 3px;
-        background-color: #ffff;
-        margin: 4px 0;
-        transition: 0.4s;
+      height: 2px;
+      width: 25px;
+      background: #101e26;
+      margin: 5px 0;
+      transition: 0.4s;
+    }
+  
+    .bar-top-open {
+      transform: translateY(12px) rotate(-50deg);
+    }
+  
+    .bar-mid-open {
+      opacity: 0;
+    }
+  
+    .bar-bottom-open {
+      transform: translateY(-12px) rotate(50deg);
+    }
+  }
+@media screen and (max-width: 768px) {
+  .nav-links {
+    flex-direction: column;
+    align-items: flex-start;
+    position: absolute;
+    top: 70px;
+    left: 0;
+    width: 100%;
+    background: #498c79;
+    display: none;
+    z-index: 1;
+  }
+
+  .nav-links .nav-open {
+    display: flex;
+    transition: all 0.5s ease-in-out;
+  }
+
+  .nav-links li {
+    width: 100%;
+    text-align: left;
+  }
+
+  .menu-toggle {
+    display: flex;
+    padding: 20px;
+    width: 100%;
+    background-color: #498c79;
+    align-items: center;
+    border-bottom-left-radius: 10%;
+    border-bottom-right-radius: 10%;
+  }
+
+  .menu-toggle:hover {
+    scale: 1.1;
+    .bar {
+      background: #f2d1b3;
+    }
+  }
+
+  .nav-open {
+    display: flex;
+    padding: 20px;
+    align-items: center;
+    transition:all 0.5s 
+  }
+
+  .nav-open li {
+    margin: 10px 0;
+
+    a {
+      font-size: 20px;
     }
 
-    .nav-active .bar:nth-child(1) {
-        transform: rotate(-45deg) translate(-5px, 6px);
+    a:hover {
+      color: #f2d1b3;
+      letter-spacing: 5px;
     }
 
-    .nav-active .bar:nth-child(2) {
-        opacity: 0;
-    }
-
-    .nav-active .bar:nth-child(3) {
-        transform: rotate(45deg) translate(-5px, -6px);
-    }
+  }
 }
 </style>
