7dab7ebc81bcac483a03d9641e21876f9fa20307
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2024-04-01 17:57:28 -0400

    update images

--- proyecto_vue/src/service/ImageService.js
+++ proyecto_vue/src/service/ImageService.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export async function uploadImage(image) {
     const url = "http://localhost:3003/upload";
     let uploaded_image_path = "";
+    console.log("uploadImage: ");
     let formData = new FormData();
     formData.append('image', image);
     await axios.post(url, formData, {
@@ -10,6 +11,7 @@ export async function uploadImage(image) {
         'Content-Type': 'multipart/form-data'
       }
     }).then(response => {
+        console.log(response.data);
         uploaded_image_path = response.data.path;
     });
 
