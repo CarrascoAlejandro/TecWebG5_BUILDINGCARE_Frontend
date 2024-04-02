c29db01ef750a5daf234099b6d85a5d6868ff6f6
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2024-04-01 22:33:03 -0400

    iamge4

--- proyecto_vue/src/service/ImageService.js
+++ proyecto_vue/src/service/ImageService.js
@@ -3,9 +3,12 @@ import axios from 'axios';
 export async function uploadImage(image) {
     const url = "http://localhost:3003/upload";
     let uploaded_image_path = "";
-    console.log("uploadImage: ");
+    console.log("uploadImage: ", image);
     let formData = new FormData();
     formData.append('image', image);
+    console.log("formData: " + formData);
+    //confirm before proceed
+    let confirm = window.confirm("Do you want to upload this image?");
     await axios.post(url, formData, {
       headers: {
         'Content-Type': 'multipart/form-data'
