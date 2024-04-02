92eca32c58829d121c9235ea1b6bed58c82997e4
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2024-04-01 23:34:15 -0400

    image13

--- image_server/image_server.mjs
+++ image_server/image_server.mjs
@@ -8,29 +8,25 @@ const app = express();
 
 app.use(cors());
 
-const __dirname = path.resolve();
+const multer = require('multer');
 
-const dirPath = path.join(__dirname, "img");
+const upload = multer({ dest: 'img/' });
 
-if (!fs.existsSync(dirPath)) {
-  fs.mkdirSync(dirPath, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    /**        cb(null, 'image_server/img')
-     */ cb(null, "img");
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + "-" + Date.now() + ".jpg");
-  },
-});
-
-const upload = multer({ storage: storage });
-
-app.post("/upload", upload.single("image"), (req, res) => {
-  console.log(req.file.filename);
-  res.json({ path: req.file.filename });
+app.post('/upload', upload.single('image'), (req, res) => {
+    console.log("uploading image");
+    console.log("req.file: ", req.file);
+    console.log("req.body: ", req.body);
+    const tempPath = req.file.path;
+    const targetPath = path.join(__dirname, "./uploads/image.png");
+    console.log("tempPath: ", tempPath);
+    console.log("targetPath: ", targetPath);
+    fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+        res
+            .status(200)
+            .contentType("text/plain")
+            .end("File uploaded!");
+    });
 });
 
 app.listen(3003, () => console.log("Image server listening on port 3003!"));
